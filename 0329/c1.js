bdd97cf2ab23b392f9c85cb54c59bf4b7162c647
noahtice1 <157340944+noahtice1@users.noreply.github.com>
2024-03-29 10:26:06 -0500

    fef

--- 0329/c1.js
+++ 0329/c1.js
@@ -1,42 +1,44 @@
 "use strict";
 
 let R = null;
+let chart2 = null;
+let b2 = document.querySelector("#b2");
 
-let b1 = document.querySelector("#b1");
-let chart1 = null; // for the chart to be shown 
-
-
-async function loadd()
+async function showc()
 {
-    //fetch()
+    // fetch() async
+    let url = `https://juxinglong.github.io/static/HW/hw6/MIS3033.json`;
 
-    let url = `https://juxinglong.github.io/static/data/states.json`;
+    //fetch(url);
     let r = await fetch(url);
     let rj = await r.json();
 
-    let c1 = document.querySelector("#c1");
-    if (chart1 != null)
-    {
-        chart1.destroy();
-    }
-    c1.innerHTML = ``; 
+    let c2 = document.querySelector("#c2");
 
     let opts = {
         type: "pie",
-        
         data: {
-            labels: rj.map(x => x.st),
-            datasets: [
-                {data:rj.map(x=>x.p),},
-            ],
+            labels:rj.map(x=>x.lg),
+            datasets: [{data:rj.map(x=>x.n),label:"Number",},],
         },
+
     };
-    chart1 = new Chart(c1, opts);
+
+    chart2 = new Chart(c2,opts);
+    if (chart2 != null) {
+        chart2.destroy();
+    }
+    c2.innerHTML = ``;
+
+    chart2 = new Chart(c2, opts);
+
+    opts = {
+        targets: [c2,],
+        rotate: { value: 60, duration: 3000 },
+    };
 
     R = rj;
     console.log(rj);
-
-    Swal.fire("Load data");
 }
 
-b1.addEventListener("click", loadd);
+b2.addEventListener("click", showc);
